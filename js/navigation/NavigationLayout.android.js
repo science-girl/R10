ac07fb79fb8e6a94660f62a5ca709f979ae36132
Lindsey <lindsey@users-MBP.hitronhub.home>
2018-03-01 21:08:34 -0800

    added purple for selected items on android drawer nav

--- js/navigation/NavigationLayout.android.js
+++ js/navigation/NavigationLayout.android.js
@@ -6,6 +6,12 @@ import {
   DrawerNavigation,
   DrawerNavigationItem
 } from "@expo/ex-navigation";
+import { colors } from "../config/styles";
+import Icon from "react-native-vector-icons/Ionicons";
+import { heartIcon, aboutIcon, calendarIcon } from "../lib/platformHelpers";
+import { styles } from "./styles";
+
+const { Black, White, MediumGrey, Purple } = colors;
 
 // Treat the DrawerNavigationLayout route like any other route -- you may want to set
 // it as the intiial route for a top-level StackNavigation
@@ -30,6 +36,7 @@ export default class NavigationLayout extends React.Component {
           id="schedule"
           selectedStyle={styles.selectedItemStyle}
           renderTitle={isSelected => this._renderTitle("Schedule", isSelected)}
+          renderIcon={isSelected => this._renderIcon(isSelected, calendarIcon)}
         >
           <StackNavigation
             id="schedule"
@@ -42,6 +49,7 @@ export default class NavigationLayout extends React.Component {
           id="about"
           selectedStyle={styles.selectedItemStyle}
           renderTitle={isSelected => this._renderTitle("About", isSelected)}
+          renderIcon={isSelected => this._renderIcon(isSelected, aboutIcon)}
         >
           <StackNavigation
             id="about"
@@ -53,6 +61,7 @@ export default class NavigationLayout extends React.Component {
           id="faves"
           selectedStyle={styles.selectedItemStyle}
           renderTitle={isSelected => this._renderTitle("Faves", isSelected)}
+          renderIcon={isSelected => this._renderIcon(isSelected, heartIcon)}
         >
           <StackNavigation
             id="faves"
@@ -77,22 +86,14 @@ export default class NavigationLayout extends React.Component {
       </Text>
     );
   }
+  _renderIcon = (isSelected, iconName) => {
+    return (
+      <Icon
+        active
+        name={iconName}
+        size={16}
+        color={isSelected ? Purple : MediumGrey}
+      />
+    );
+  };
 }
-
-const styles = StyleSheet.create({
-  header: {
-    height: 20
-  },
-
-  selectedItemStyle: {
-    backgroundColor: "blue"
-  },
-
-  titleText: {
-    fontWeight: "bold"
-  },
-
-  selectedTitleText: {
-    color: "black"
-  }
-});
--- js/navigation/styles.js
+++ js/navigation/styles.js
@@ -1,9 +1,25 @@
 import { StyleSheet } from "react-native";
 import { colors, typography } from "../config/styles";
-
 export const styles = StyleSheet.create({
   navBarTitles: {
     fontSize: 10,
+    fontFamily: typography.fontMain
+  },
+  header: {
+    height: 20
+  },
+
+  selectedItemStyle: {
+    backgroundColor: colors.LightGrey
+  },
+
+  titleText: {
+    fontFamily: typography.fontMain,
+    padding: 10
+  },
+
+  selectedTitleText: {
+    color: colors.Purple,
     fontFamily: typography.fontMain
   }
 });
